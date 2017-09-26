73dfb07ff946af9ec088557ce88e693429e3a52e
TonyStew <bino4444@gmail.com>
2017-09-26 15:55:14 -0400

    updating the naming to add more sorting methods

--- src/SortMain.java
+++ src/SortMain.java
@@ -3,13 +3,13 @@ import java.util.ArrayList;
 public class SortMain {
 
     public static void main(String[] args){
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(5);
-        list.add(23);
-        list.add(12);
-        list.add(6);
+        ArrayList<Double> list = new ArrayList<>();
+        list.add(5.2);
+        list.add(5.1);
+        list.add(6.0);
+        list.add(10.6);
 
         System.out.println(list);
-        System.out.println(new ThreadSorter<Integer>().sort(list));
+        System.out.println(new Sorter<Double>().sleepSort(list));
     }
 }
--- src/Sorter.java
+++ src/Sorter.java
@@ -4,18 +4,22 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-public class ThreadSorter<T extends Number>{ //I used generics so that this class works with floats, longs, etc on top of ints
+public class Sorter<T extends Number>{ //I used generics so that this class works with floats, longs, etc on top of ints
     private ArrayList<T> list;
 
-    public ThreadSorter(){ list = new ArrayList<>(); }
+    public Sorter(){ list = new ArrayList<>(); }
 
-    synchronized Collection<T> sort(Collection<T> inputList){
+    public Collection<T> sleepSort(Collection<T> inputList){ return sleepSort(inputList, 100); }
+
+    public synchronized Collection<T> sleepSort(Collection<T> inputList, int precision){
         ScheduledExecutorService executorService = Executors.newScheduledThreadPool(inputList.size());
         for(T t: inputList) {
-            executorService.schedule(() -> list.add(t), t.longValue() * 100, TimeUnit.MICROSECONDS); //using microseconds with a 100 times multiple is 10 time faster than milliseconds
+            executorService.schedule(() -> list.add(t), t.longValue() * precision, TimeUnit.MICROSECONDS); //using microseconds with a 100 times multiple is 10 time faster than milliseconds
         }                                                                                                 //any faster than this and you start to have concurrent variable modification errors and other dumb thread stuff
         while(true){
             if(list.size() == inputList.size()) return list;
         }
     }
+
+
 }
